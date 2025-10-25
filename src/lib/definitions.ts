084165853d075d47cd0acf4ea7c28fe0e0193350
Brent Coughenour <brent.cokenr@gmail.com>
2025-10-25 16:51:13 -0700

    reinstating necessary definitions

--- src/lib/definitions.ts
+++ src/lib/definitions.ts
@@ -1,5 +1,8 @@
-import { Prisma } from "@prisma/client";
+import { Prisma } from '@prisma/client';
 
+export type Item = Prisma.ItemGetPayload<{ include: { categories: true } }>;
+export type Transaction = Prisma.TransactionGetPayload<{ include: { items: true } }>;
+export type Category = Prisma.CategoryGetPayload<{ include: { items: true } }>;
 export type ItemWithCategories = Prisma.ItemGetPayload<{
   include: { categories: true };
 }>;
