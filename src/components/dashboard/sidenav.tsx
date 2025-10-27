183d36df824e5a3fc82be55c4f5bba559c893bfd
Brent Coughenour <brent.cokenr@gmail.com>
2025-10-27 15:24:02 -0700

    adding image in sidebar

--- src/components/dashboard/sidenav.tsx
+++ src/components/dashboard/sidenav.tsx
@@ -2,14 +2,16 @@ import Link from 'next/link';
 import NavLinks from '@/components/dashboard/nav-links';
 import { PowerIcon } from '@heroicons/react/24/outline';
 import { signOut } from '@/auth';
+import styles from "@/styles/home.module.css";
 
 export default function SideNav() {
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2 sideNav">
       <Link
-        className={`mb-2 flex h-20 items-end justify-start rounded-md p-4 md:h-40`}
+        className={`${styles.bannerLogo} mb-2 flex h-20 items-end justify-start rounded-md p-4 md:h-40`}
         href="/"
       >
+        InBrentory
       </Link>
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
         <NavLinks />
