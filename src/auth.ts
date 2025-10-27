84ae759aad5f1de96073fc79c14c55a4fb9af2cf
Brent Coughenour <brent.cokenr@gmail.com>
2025-10-26 17:11:37 -0700

    adding auth route handler

--- src/auth.ts
+++ src/auth.ts
@@ -1,41 +1,36 @@
-import NextAuth from 'next-auth';
-import { authConfig } from './auth.config';
-import Credentials from 'next-auth/providers/credentials';
-import { z } from 'zod';
-import type { Admin } from '@/lib/definitions';
-import bcrypt from 'bcrypt';
-import { prisma } from '@/lib/prisma';
+import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
+import { prisma } from "@/lib/prisma";
+import { z } from "zod";
 
-export async function getAdmin(email: string): Promise<Admin | null> {
-  try {
-    return await prisma.admin.findUnique({ where: { email } });
-  } catch (error) {
-    console.error('Database Error:', error);
-    throw new Error('Failed to fetch admin user');
-  }
-}
+export const authConfig = {
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const parsed = z
+          .object({
+            email: z.string().email(),
+            password: z.string().min(6),
+          })
+          .safeParse(credentials);
 
-export const { auth, signIn, signOut } = NextAuth({
-    ...authConfig,
-    providers: [
-        Credentials({
-            async authorize(credentials) {
-                const parsedCredentials = z
-                    .object({ email: z.email(), password: z.string().min(6) })
-                    .safeParse(credentials);
-                console.log(parsedCredentials);
-                if (parsedCredentials.success) {
-                    const { email, password } = parsedCredentials.data;
-                    const user = await getAdmin(email);
-                    if (!user) return null;
-                    const passwordsMatch = await bcrypt.compare(password, user.password);
-                    console.log('passwords match: ', passwordsMatch);
-                    if (passwordsMatch) return user;
-                }
+        if (!parsed.success) return null;
 
-                console.log('Invalid credentials');
-                return null;
-            },
-        }),
-    ],
-});
+        const { email, password } = parsed.data;
+        const user = await prisma.admin.findUnique({ where: { email } });
+        if (!user) return null;
+
+        const passwordsMatch = await bcrypt.compare(password, user.password);
+        if (!passwordsMatch) return null;
+
+        return { id: user.id, email: user.email };
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/login",
+  },
+};
+
+export const { auth, signIn, signOut, handlers } = NextAuth(authConfig);
