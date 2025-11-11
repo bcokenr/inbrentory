1476b185687dcabe0a09bb9a7895eea58aad9dc1
Brent Coughenour <brent.cokenr@gmail.com>
2025-11-11 12:53:08 -0800

    updating dashboard font

--- src/components/dashboard/date-range-chart.tsx
+++ src/components/dashboard/date-range-chart.tsx
@@ -1,9 +1,11 @@
 import { DateRangePicker } from "@/components/date-range-picker";
 import { SalesChart } from "@/components/dashboard/sales-chart";
+import styles from '@/styles/home.module.css';
+
 
 export function DateRangeChart({ data }: { data: { date: string; total: number }[] }) {
     return (
-        <div className="space-y-6">
+        <div className={[styles.sometypeMono, "space-y-6"].join(" ")}>
             <h1 className="text-xl font-semibold">Sales (Daily)</h1>
             <DateRangePicker />
             <SalesChart data={data} />
